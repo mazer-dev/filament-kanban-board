af280ad1fb4bb7a8074862272cbeea1ce44b0eea
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2025-09-03 18:37:27 -0300

    feat: header step description

--- resources/views/kanban-step.blade.php
+++ resources/views/kanban-step.blade.php
@@ -1,18 +1,32 @@
+@php
+    use Filament\Support\Icons\Heroicon;
+    use Filament\Support\Colors\Color;
+@endphp
+
 <div class="kanban-column" data-step-id="{{ $step->id }}">
     <header class="kanban-column-header">
         <h3 class="kanban-column-title">
             {{ $step->{static::$stepTitleAttribute} }}
-            <span class="kanban-column-count">
-                {{ count($cards) ?? 0 }}
-            </span>
-            <button class="kanban-toggle-collapse">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
-                          d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z"
-                          clip-rule="evenodd"></path>
-                </svg>
-            </button>
         </h3>
+
+        <x-filament::badge>
+            {{ count($cards) ?? 0 }}
+        </x-filament::badge>
+
+        @if( $step[static::$stepDescriptionAttribute] )
+            <x-filament::icon-button
+                    :icon="Heroicon::InformationCircle"
+                    color="gray"
+                    tooltip="{{ $step[static::$stepDescriptionAttribute] }}"
+            />
+        @endif
+
+        <x-filament::icon-button
+            :icon="Heroicon::ChevronDoubleLeft"
+            color="gray"
+            :title="__('Contrair a coluna') "
+        />
+
     </header>
 
     <div class="kanban-column-content"
