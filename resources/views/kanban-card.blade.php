a03801791a357e2860ade6beca88d606810a6d36
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2025-09-05 21:15:46 -0300

    refact: remove unsed code

--- resources/views/kanban-card.blade.php
+++ resources/views/kanban-card.blade.php
@@ -4,47 +4,14 @@
      :key="{{ $card->id }}"
 >
 
-    @if($card->priority)
-        <div class="kanban-card-priority kanban-card-priority--{{ $card->priority }}"></div>
-    @endif
-
     <div class="kanban-card-content">
         {{ $this->renderCardInfoList($card, new \Filament\Schemas\Schema($this)) }}
-
-        @if($card->description)
-            <p class="kanban-card-description">
-                {{ Str::limit($card->description, 100) }}
-            </p>
-        @endif
     </div>
-
-    @if($card->tags?->count() > 0)
-        <div class="kanban-card-tags">
-            @foreach($card->tags as $tag)
-                <span class="kanban-card-tag" style="background-color: {{ $tag->color }}">
-                    {{ $tag->name }}
-                </span>
-            @endforeach
-        </div>
-    @endif
 
     <footer class="kanban-card-footer">
 {{--        @foreach($this->getCardFooterActions() as $action)--}}
 {{--            @ds($card->id)--}}
 {{--            {{ ($action)(['cardId' => $card->id]) }}--}}
 {{--        @endforeach--}}
-
-        @if($card->due_date)
-            <span class="kanban-card-date {{ $card->is_overdue ? 'kanban-card-date--overdue' : '' }}">
-                <x-heroicon-m-calendar class="kanban-card-date-icon"/>
-                {{ $card->due_date->format('d/m') }}
-            </span>
-        @endif
-
-        @if($card->assignee)
-            <img src="{{ $card->assignee->avatar_url }}"
-                 alt="{{ $card->assignee->name }}"
-                 class="kanban-card-avatar">
-        @endif
     </footer>
 </div>
